69a858cd52aaee0e02c3b0b55b7ed5c736672ded
Rakshit <34887766+RakshitTra@users.noreply.github.com>
2020-07-03 19:25:23 +0530

    Create template.cpp
    Template for competitive programming

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(),(v).end()
+#define mp make_pair
+#define mset(a, v) memset(a, v, sizeof(a))
+#define long long ll
+
+#define deb(x) cout << '>' << #x << ':' << x << endl;
+#define debarr(a,n)cerr<<#a<<":";for(int i=0;i<n;i++)cerr<<a[i]<<" ";cerr<<endl;
+#define debpr(a) cout<<#a<<" = ( "<<((a).first)<<" , "<<((a).second)<<" )\n";
+#define deb1(x)             cerr<<#x<<"="<<x<<endl
+#define deb2(x, y)          cerr<<#x<<"="<<x<<" "<<#y<<"="<<y<<endl
+#define deb3(x, y, z)       cerr<<#x<<"=" <<x<<" "<<#y<<"="<<y<<" "<<#z<<"="<<z<<endl
+#define deb4(a,b,c,d)       cerr<<#a<<"="<<a<<" "<<#b<<"="<<b<<" "<<#c<<"="<<c<<" "<<#d<<"="<<d<<endl
+#define deb5(a,b,c,d,e)     cerr<<#a<<"="<<a<<" "<<#b<<"="<<b<<" "<<#c<<"="<<c<<" "<<#d<<"="<<d<<" "<<#e<<"="<<e<<endl
+#define deb6(a,b,c,d,e,f)   cerr<<#a<<"="<<a<<" "<<#b<<"="<<b<<" "<<#c<<"="<<c<<" "<<#d<<"="<<d<<" "<<#e<<"="<<e<<" "<<#f<<"="<<f<<endl
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+#define MOD 1000000007
+
+//Template ends
